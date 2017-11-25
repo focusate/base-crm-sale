24ba8ea9cbb6afdf902a1e3da559059678dc71ec
Andrius Laukavičius <andrius@focusate.eu>
2017-11-25 19:50:12 +0200

    [REF] smtp_per_user: bump version to 1.0.0

--- smtp_per_user/__manifest__.py
+++ smtp_per_user/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     'name': "SMTP Per User",
-    'version': '0.2.0',
+    'version': '1.0.0',
     'summary': 'Send letters from Odoo using your own mail',
     'category': 'Mail',
     'description': """Can configure different mail servers per user""",
